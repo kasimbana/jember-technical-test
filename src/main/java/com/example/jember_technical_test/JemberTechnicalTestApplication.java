9f5fb101f4f15266e9f9faf8775e59429e6486ca
Kevin Simbana Morocho <ka.simbana@outlook.es>
2024-09-03 00:34:21 +0200

    initial commit

---
+++ src/main/java/com/example/jember_technical_test/JemberTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jember_technical_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JemberTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JemberTechnicalTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jember_technical_test/JemberTechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jember_technical_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JemberTechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
